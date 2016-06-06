d49621ce5679ab73201dc6e942d0a6444f4add18
Amo Wu <amo260@gmail.com>
2016-06-06 17:20:35 +0800

    refactor(game): update game layer to fixed position and hide overflow

--- src/pages/Game/index.vue
+++ src/pages/Game/index.vue
@@ -1,3 +1,13 @@
+<style lang="stylus">
+  #game-layer
+    position fixed
+    top 0
+    left 0
+    right 0
+    bottom 0
+    overflow hidden
+</style>
+
 <script>
   import 'pixi'
   import 'p2'
@@ -13,12 +23,14 @@
       }
     },
     ready () {
+      const element = document.getElementById('game')
+
       const TILE_SIZE = 16
       const SCALE_RATIO = 3
       const width = Math.floor((window.innerWidth / (TILE_SIZE * SCALE_RATIO)) * TILE_SIZE)
       const height = Math.floor((window.innerHeight / (TILE_SIZE * SCALE_RATIO)) * TILE_SIZE)
 
-      const game = new Game(width, height, document.getElementById('game'))// eslint-disable-line
+      const game = new Game(width, height, element)// eslint-disable-line
 
       game.actions = new Phaser.Signal()
       game.actions.add(action => {
@@ -30,6 +42,7 @@
   }
 </script>
 
-<template>
-  <div id='game'></div>
+<template lang="jade">
+  #game-layer
+    #game
 </template>
