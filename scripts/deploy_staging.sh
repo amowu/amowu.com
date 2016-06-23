c158737dbe484d1a20c11eb25e435100bd001b8a
Amo Wu <amo260@gmail.com>
2016-06-24 01:17:25 +0800

    feat(backend): add production env CI

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,9 +1,3 @@
-STAGE=dev
+export SERVERLESS_STAGE="dev"
 
-OPTION="-s $STAGE -r $AWS_REGION"
-
-cd src/backend
-
-serverless resources deploy -p $AWS_PROFILE $OPTION
-serverless function deploy -p $AWS_PROFILE $OPTION
-serverless endpoint deploy -p $AWS_PROFILE $OPTION
+bash scripts/deploy_serverless.sh
