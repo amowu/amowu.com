53eee63c50822d467cdadff3ba6c7f4c3bdf16d7
Amo Wu <amo260@gmail.com>
2016-06-25 13:24:25 +0800

    feat(backend): endpoint support CORS
    Use serverless-cors-plugin to support CORS for AWS API Gateway.
    Setting `Access-Control-Allow-Origin` = `*` for all doamin, because it
    can not support multiple doamin.
    Need use `$ serverless endpoint deploy --all` to add OPTION method for endpoint.

--- scripts/deploy_serverless.sh
+++ scripts/deploy_serverless.sh
@@ -4,4 +4,4 @@ cd src/backend
 
 serverless resources deploy -p $AWS_PROFILE $OPTION
 serverless function deploy -p $AWS_PROFILE $OPTION
-serverless endpoint deploy -p $AWS_PROFILE $OPTION
+serverless endpoint deploy -p $AWS_PROFILE $OPTION --all
