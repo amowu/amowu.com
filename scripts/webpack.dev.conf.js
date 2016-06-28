09980630deacd2079bf3519d015fe925900da850
Amo Wu <amo260@gmail.com>
2016-06-28 14:57:28 +0800

    refactor: move webpack build file to scripts folder

--- scripts/webpack.dev.conf.js
+++ scripts/webpack.dev.conf.js
@@ -6,7 +6,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+  baseWebpackConfig.entry[name] = ['./scripts/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
