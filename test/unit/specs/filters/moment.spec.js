abec2a4033a4f8888e4891da9755595a13359317
Amo Wu <amo260@gmail.com>
2016-07-12 15:26:57 +0800

    fix: moment.js locale format
    Close #63

--- test/unit/specs/filters/moment.spec.js
+++ test/unit/specs/filters/moment.spec.js
@@ -5,6 +5,6 @@ describe('moment.filter', () => {
     expect(locale('2011-04-11')).to.equal('2011年4月11日')
   })
   it('should return correct elapsed time when given start and end date', () => {
-    expect(total('2011-04-11', '2014-08-29')).to.equal('3年')
+    expect(total('2011-04-11', '2014-08-29')).to.match(/^3(\s?)年$/)
   })
 })
