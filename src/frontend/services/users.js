2969a94f39c91a967ca54c82274910ebcb199471
Amo Wu <amo260@gmail.com>
2016-07-13 02:05:40 +0800

    refactor: update vue-resource to v0.9.3

--- src/frontend/services/users.js
+++ src/frontend/services/users.js
@@ -2,7 +2,7 @@ import resource from '../core/resource'
 import getAuthHeader from '../utils/getAuthHeader'
 
 export const getUserResume = function (userId) {
-  return resource.get(`users/${userId}/resume`, {}, {
+  return resource.get(`users/${userId}/resume`, {
     headers: getAuthHeader()
   })
 }
