f623d21409bea8b08a2c34bb9f68c5e9d5f44e62
Amo Wu <amo260@gmail.com>
2016-07-15 18:36:38 +0800

    refactor: move Auth0 params to env

--- src/backend/functions/auth/auth0Authorizer/handler.js
+++ src/backend/functions/auth/auth0Authorizer/handler.js
@@ -4,8 +4,8 @@ const AUTH0_TOKEN_LENGTH = 16
 const AuthPolicy = require('aws-auth-policy')
 const AuthenticationClient = require('auth0').AuthenticationClient
 const auth0 = new AuthenticationClient({
-  domain: 'amowu.auth0.com',
-  clientId: 'oEmGgFwDWAwQeu3y1qSWgxqFkrmlNduZ'
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID
 })
 
 module.exports.handler = function (event, context, callback) {
